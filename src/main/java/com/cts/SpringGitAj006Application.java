f2e781b10ce180d90bd8a35d8d769704e021fffa
shihaji <shihajig@gmail.com>
2021-03-05 16:57:10 +0530

    initial commit

---
+++ src/main/java/com/cts/SpringGitAj006Application.java
@@ -0,0 +1,13 @@
+package com.cts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGitAj006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGitAj006Application.class, args);
+	}
+
+}
