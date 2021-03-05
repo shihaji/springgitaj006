149d1447caeb8a5dc67feee27f406519815bf4a9
shihaji <shihajig@gmail.com>
2021-03-05 17:24:50 +0530

    added annotation and method

--- src/main/java/com/cts/controller/LoginController.java
+++ src/main/java/com/cts/controller/LoginController.java
@@ -1,5 +1,15 @@
 package com.cts.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LoginController {
+	
+	@GetMapping("index")
+	public String login() {
+		
+		return "index";
+	}
 
 }
